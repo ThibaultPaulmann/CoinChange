1076146a211a3f0eb1e360a19e24c57510c54c09
Thibault Paulmann <thibault.paulmann@gmail.com>
2019-08-28 00:58:40 +0200

    First commit of layout of code. Introduced breakdown of coin into [2,1,0.5,0.2]. Bug where coin of 0.2 does not break down.

--- CoinChange/CoinChange/Source.cpp
+++ CoinChange/CoinChange/Source.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
+
 int main() {
-	std::cout << "Hello World!";
+	
+	double coinInput;
+	// Input from user for coinValue they want to breakdown
+	// use coinInput and using for() loops break it down
+	// Should use coins of values [2,1,0.5,0.1,0.05,0.02,0.01]
+	// Output the breakdown of coinInput into coinChange.
+
+	std::cout << "Input coin: ";
+	std::cin >> coinInput;
+
+	while(coinInput >= 2) {
+		coinInput -= 2;
+	}
+
+	while (coinInput >= 1) {
+		coinInput -= 1;
+	}
+
+	while (coinInput >= 0.5) {
+		coinInput -= 0.5;
+	}
+	
+	while (coinInput >= 0.2)
+	{
+		coinInput -= 0.2;
+	}
+	std::cout << coinInput;
+
 	return 0;
 }
